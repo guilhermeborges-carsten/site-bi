0949c867c244431d49d1bc8183f49e59c86ea2d0
guilhermeborges-carsten <guilherme.borges@carsten.com.br>
2025-07-28 09:52:24 -0300

    Add files via upload

---
+++ siteBI/start_flask.bat
@@ -0,0 +1,12 @@
+@echo off
+REM Ativa o ambiente virtual se existir
+IF EXIST venv (call venv\Scripts\activate)
+
+REM Define a variável de ambiente do Flask
+set FLASK_APP=app.py
+set FLASK_ENV=production
+
+REM Inicia o servidor Flask na porta 8000
+flask run --host=0.0.0.0 --port=8000
+
+pause 
