29326fa13946f97104953341bd04cdc67c6e645d
guilhermeborges-carsten <guilherme.borges@carsten.com.br>
2025-08-18 15:45:56 -0300

    Add files via upload

--- siteBI/config.py
+++ siteBI/config.py
@@ -7,7 +7,12 @@ class Config:
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16MB
     
     # Configuração do banco MySQL
-  
+    # Altere estas configurações conforme seu ambiente
+    MYSQL_HOST = '46.202.151.75'
+    MYSQL_PORT = 3306
+    MYSQL_USER = 'user_bi'
+    MYSQL_PASSWORD = 'sk15iY4rVGLCoqK0'  # Deixe vazio se não tiver senha
+    MYSQL_DATABASE = 'BI'
     
     @staticmethod
     def init_app(app):
@@ -34,4 +39,3 @@ config = {
     'production': ProductionConfig,
     'default': DevelopmentConfig
 }
-
